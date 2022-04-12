59632edeee473033fb27bb5c4623dc09ea01bb0b
elisttm <61097889+elisttm@users.noreply.github.com>
2022-04-12 13:34:35 +0000

    complete upgrade to everything

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import quart, asyncio, hypercorn
 from quart import redirect, url_for
-from markupsafe import Markup
 
 app = quart.Quart(__name__)
 
 @app.route('/')
 async def index(): 
 	return await quart.render_template('index.html')
+
+@app.route('/about')
+async def about(): 
+	return await quart.render_template('about.html')
 
 @app.route('/eli')
 async def sona(): 
@@ -26,7 +29,7 @@ async def pack():
 
 @app.route('/minecraft')
 async def minecraft():
-	return await quart.render_template('minecraft.html')
+	return await quart.render_template('extra/minecraft.html')
 
 @app.route('/gmod')
 async def gmod_shortcut():
@@ -43,18 +46,6 @@ async def source_motd():
 async def static_from_root(): 
 	return await quart.send_from_directory(app.static_folder, quart.request.path[1:])
 
-@app.errorhandler(403)
-async def forbidden(error): 
-	return await quart.render_template('extra/error.html', e=["[403] access denied","you do not have the proper authorization to view this page"]), 403
-
-@app.errorhandler(404)
-async def page_not_found(error): 
-	return await quart.render_template('extra/error.html', e=["[404] page not found",Markup('the url youre trying to access does not exist, either i screwed something up or you got the wrong link')]), 404
-
-@app.errorhandler(500)
-async def internal_server(error): 
-	return await quart.render_template('extra/error.html', e=["[500] internal server error","somewhere along the way i screwed something up and there was an error in processing your request",]), 500
-
 @app.route('/trashbot')
 async def trashbot_redirect(): 
   return redirect(url_for('elibot'), code=301)
@@ -62,6 +53,18 @@ async def trashbot_redirect():
 @app.route('/sona')
 async def sona_redirect(): 
   return redirect(url_for('sona'), code=308)
+
+@app.errorhandler(403)
+async def forbidden(error): 
+	return await quart.render_template('extra/error.html', e=["[403] access denied","you do not have the proper authorization to view this page"]), 403
+
+@app.errorhandler(404)
+async def page_not_found(error): 
+	return await quart.render_template('extra/error.html', e=["[404] page not found",'the url youre trying to access does not exist, either i screwed something up or you got the wrong link']), 404
+
+@app.errorhandler(500)
+async def internal_server(error): 
+	return await quart.render_template('extra/error.html', e=["[500] internal server error","somewhere along the way i screwed something up and there was an error in processing your request",]), 500
 
 hyperconfig = hypercorn.config.Config()
 hyperconfig.bind = ["0.0.0.0:8080"]
